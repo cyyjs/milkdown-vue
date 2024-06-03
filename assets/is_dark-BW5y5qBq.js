025782d87906c5c8c2c565484be3874c109502d4
cyyjs <cyyjs@cyyjs.top>
2024-06-03 04:57:54 +0000

    Deploying to gh-pages from @ cyyjs/milkdown-vue@22e5de631578eedfd7c103232852659e8e11d7a0 🚀

---
+++ assets/is_dark-BW5y5qBq.js
@@ -0,0 +1,1 @@
+import{C as s,U as r}from"./index-DsOH8oTa.js";const i=n=>{const{r:t,g:a,b:o}=s.parse(n),e=.2126*r.channel.toLinear(t)+.7152*r.channel.toLinear(a)+.0722*r.channel.toLinear(o);return r.lang.round(e)},c=n=>i(n)>=.5,u=n=>!c(n);export{u as i};
